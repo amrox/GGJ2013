10af2ff099ae327067b6db77b21a41cb3a85fc80
Tedo Salim <tedo.salim@gree.co.jp>
2013-01-26 14:07:50 -0800

    + added placeholder background
    Signed-off-by: Tedo Salim <tedo.salim@gree.co.jp>

--- rant/rant/GameBackgroundLayer.m
+++ rant/rant/GameBackgroundLayer.m
@@ -8,4 +8,12 @@
 
 @implementation GameBackgroundLayer
 
+- (void)onEnter
+{
+    CCSprite *background = [CCSprite spriteWithFile:@"gameBackground_placeholder.png"];
+    [background setPosition:ccp(160, 240)];
+    
+    [self addChild:background];
+}
+
 @end
