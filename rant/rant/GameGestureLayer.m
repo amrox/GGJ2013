ba50b6462c5adb4909ae8d729480ee44670fffcb
Greg Maxey <greg.maxey@gree.co.jp>
2013-01-26 12:37:56 -0800

    touches

--- rant/rant/GameGestureLayer.m
+++ rant/rant/GameGestureLayer.m
@@ -8,4 +8,28 @@
 
 @implementation GameGestureLayer
 
+-(void)onEnter
+{
+	self.isTouchEnabled = YES;
+	CCDirector *director = [CCDirector sharedDirector];
+	[[director touchDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
+}
+
+
+- (BOOL)ccTouchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	NSLog(@"1");
+	return YES;
+}
+
+- (void)ccTouchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	NSLog(@"2");
+}
+
+- (void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+{
+	NSLog(@"3");
+}
+
 @end
--- rant/rant/GameScene.m
+++ rant/rant/GameScene.m
@@ -7,36 +7,6 @@
 #import "GameHUDLayer.h"
 
 
-#define RANT_FONT @"Bernard MT Condensed"
-
-
-@interface GameLayer : CCLayer
-{
-}
-
-@end
-
-
-
-@implementation GameLayer
-{
-}
-
--(void)onEnter
-{
-    // Create the layer hierarchy
-    [super onEnter];
-    
-    [self addChild:[GameBackgroundLayer node]];
-    [self addChild:[GameMonsterLayer node]];
-    [self addChild:[GameHUDLayer node]];
-    [self addChild:[GameGestureLayer node]];
-}
-
-@end
-
-
-
 @implementation GameScene
 
 +(CCScene *) scene
@@ -46,9 +16,14 @@
 	return scene;
 }
 
--(void) onEnter
+-(void)onEnter
 {
-	[super onEnter];
+    [super onEnter];
+
+    [self addChild:[GameBackgroundLayer node]];
+    [self addChild:[GameMonsterLayer node]];
+    [self addChild:[GameHUDLayer node]];
+    [self addChild:[GameGestureLayer node]];
 }
 
 @end
