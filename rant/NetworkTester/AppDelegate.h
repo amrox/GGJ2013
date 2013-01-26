f0ddd8f8c812bd54a0ccfc550177d72361670e7b
Andy Mroczkowski <andy@mrox.net>
2013-01-25 22:29:06 -0800

    Adding more skeleton stuff

---
+++ rant/NetworkTester/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NetworkTester
+//
+//  Created by Andy Mroczkowski on 1/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rant/NetworkTester/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetworkTester
+//
+//  Created by Andy Mroczkowski on 1/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
