984418992b1c7ddb47b9573cba89693f9dfe25aa
Greg Maxey <greg.maxey@gree.co.jp>
2013-01-26 12:10:38 -0800

    asfd

---
+++ rant/rant/MainMenuScene.h
@@ -0,0 +1,9 @@
+#import "cocos2d.h"
+
+@interface MainMenuScene : CCScene
+{
+}
+
++(CCScene *) scene;
+
+@end
---
+++ rant/rant/ResultsScene.m
@@ -0,0 +1,78 @@
+#import "ResultsScene.h"
+#import "MainMenuScene.h"
+
+
+#define RANT_FONT @"Bernard MT Condensed"
+
+
+
+@interface ResultsLayer : CCLayer
+{
+}
+
+@end
+
+
+
+@implementation ResultsLayer
+{
+	CCMenuItemImage * enterGameButton;
+	CCLabelTTF * enterGameLabel;
+}
+
+-(void)onEnter
+{
+    // Create the layer hierarchy
+    [super onEnter];
+
+	// ask director for the window size
+//	CGSize size = [[CCDirector sharedDirector] winSize];
+
+    enterGameButton = [CCMenuItemImage itemWithNormalImage:@"start-menu-button.png"
+										  selectedImage:@"start-menu-button-pressed.png"
+												 target:self
+											   selector:@selector(enterGamePressed:)];
+
+    CGPoint savedPoint = ccp([enterGameButton boundingBox].size.width * 0.5f,
+                             [enterGameButton boundingBox].size.height * 0.5f);
+
+    [enterGameButton setPosition:ccp(0,-100)];
+    enterGameLabel = [CCLabelTTF labelWithString:@"" fontName:RANT_FONT fontSize:26];
+    [enterGameButton addChild:enterGameLabel];
+    [enterGameLabel setPosition:savedPoint];
+
+	CCMenu *menu = [CCMenu menuWithItems:enterGameButton, nil];
+    [self addChild:menu];
+}
+
+- (void)enterGamePressed:(id)sender
+{
+	[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[MainMenuScene scene] withColor:ccWHITE]];
+}
+
+@end
+
+
+
+@implementation ResultsScene
+
++(CCScene *) scene
+{
+	CCScene *scene = [ResultsScene node];
+
+	ResultsLayer *layer = [ResultsLayer node];
+
+	[scene addChild: layer];
+
+	return scene;
+}
+
+-(void) onEnter
+{
+	[super onEnter];
+}
+
+@end
+
+
+
