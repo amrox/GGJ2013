22376ad9cfdc9999d61421e633c832335dbba505
Tedo Salim <tedo.salim@gree.co.jp>
2013-01-26 20:03:16 -0800

    ~ moved stuff in gestureLayer around a bit
    Signed-off-by: Tedo Salim <tedo.salim@gree.co.jp>

--- rant/rant/GameGestureLayer.m
+++ rant/rant/GameGestureLayer.m
@@ -10,17 +10,6 @@
 
 #define PI 3.141592653589
 
-typedef enum
-{
-	EGesture_NONE,
-	EGesture_WATER,		//figure 8
-	EGesture_FIRE,		//z with return
-	EGesture_AIR,		//square
-	EGesture_ATTACK,	//stab
-	EGesture_HEAL,		//backwards c
-} EGesture;
-
-
 #define DIST_FOR_ACCURATE_ANGLE1 20
 #define DIST_FOR_ACCURATE_ANGLE2 80
 #define ANGLE_DIFFERENCE_FOR_NEW_LEG 0.5f
@@ -53,19 +42,6 @@ float getDifferenceBetweenAngles(float a1, float a2)
 	}
 	return diff;
 }
-
-
-
-@interface Gesture : NSObject
-
-@property (nonatomic, strong) NSArray * legAngles;
-@property (nonatomic, assign) EGesture gesture;
-
-- (BOOL)matchesLegAngles:(float[])legAngles numLegs:(int)numLegs;
-
-+ (NSArray*)gestureLibrary;
-
-@end
 
 @implementation Gesture
 
