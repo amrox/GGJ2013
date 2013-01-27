23b1a0228e1aecc971dd5ac755fa1ab59677e700
Greg Maxey <greg.maxey@gree.co.jp>
2013-01-26 16:43:34 -0800

    chained gestures and comparing against gesture library works.

--- rant/rant/GameGestureLayer.m
+++ rant/rant/GameGestureLayer.m
@@ -17,59 +17,23 @@ typedef enum
 } EGesture;
 
 
-@interface Gesture : NSObject
-
-@end
-
-@implementation Gesture
-
-@end
-
-
-@interface GestureRecognizer : NSObject
-
-- (id)initAtStartingPos:(CGPoint)startingPos;
-- (void)newTouchAt:(CGPoint)pos;
-
-- (EGesture)getGesture;
-
-@end
-
-
-
+#define DIST_FOR_ACCURATE_ANGLE 60
+#define ANGLE_DIFFERENCE_FOR_NEW_LEG (PI/3.0f)
 #define MAX_POINTS 10000
-#define MAX_LEGS 100
+#define MAX_LEGS 8
 
-@implementation GestureRecognizer
-{
-	CGPoint points[MAX_POINTS];
-	int numPoints;
 
-	float legAngles[MAX_LEGS];
-	int numLegs;
-}
-
-- (id)initAtStartingPos:(CGPoint)startingPos
-{
-	if (self = [super init])
-	{
-		points[0] = startingPos;
-		numPoints = 1;
-	}
-	return self;
-}
-
-- (float)angleBetweenPoint:(CGPoint)p1 and:(CGPoint)p2
+float angleBetweenPoints(CGPoint p1, CGPoint p2)
 {
 	return atan2f(p2.y - p1.y, p2.x - p1.x);
 }
 
-- (float)distanceBetweenPoint:(CGPoint)p1 and:(CGPoint)p2
+float distanceBetweenPoints(CGPoint p1, CGPoint p2)
 {
 	return sqrtf((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
 }
 
-- (float)getDifferenceBetweenAngle:(float)a1 and:(float)a2
+float getDifferenceBetweenAngles(float a1, float a2)
 {
 	float diff = a2 - a1;
 	while (diff > PI)
@@ -83,8 +47,97 @@ typedef enum
 	return diff;
 }
 
-#define DIST_FOR_ACCURATE_ANGLE 60
-#define ANGLE_DIFFERENCE_FOR_NEW_LEG (PI/3.0f)
+
+
+@interface Gesture : NSObject
+
+@property (nonatomic, strong) NSArray * legAngles;
+@property (nonatomic, assign) EGesture gesture;
+
+- (BOOL)matchesLegAngles:(float[])legAngles numLegs:(int)numLegs;
+
++ (NSArray*)gestureLibrary;
+
+@end
+
+@implementation Gesture
+
+- (BOOL)matchesLegAngles:(float[])legAnglesArray numLegs:(int)numLegs
+{
+	if (numLegs == 0 || numLegs != [self.legAngles count])
+	{
+		return NO;
+	}
+	for (int i = 0; i < numLegs; i++)
+	{
+		NSNumber * legAngle = [self.legAngles objectAtIndex:i];
+		float angleDiff = getDifferenceBetweenAngles([legAngle floatValue], legAnglesArray[i]);
+		if (fabsf(angleDiff) > ANGLE_DIFFERENCE_FOR_NEW_LEG)
+		{
+			return NO;
+		}
+	}
+	return YES;
+}
+
++ (NSArray*)gestureLibrary
+{
+	static NSMutableArray * sharedLibrary = nil;
+	if (!sharedLibrary)
+	{
+		sharedLibrary = [NSMutableArray array];
+
+		Gesture * fire = [[Gesture alloc] init];
+		fire.gesture = EGesture_FIRE;
+		fire.legAngles = [NSArray arrayWithObjects:[NSNumber numberWithFloat:PI*0.5f], [NSNumber numberWithFloat:-PI*0.5f], nil];
+		[sharedLibrary addObject:fire];
+
+		Gesture * water = [[Gesture alloc] init];
+		water.gesture = EGesture_WATER;
+		water.legAngles = [NSArray arrayWithObjects:[NSNumber numberWithFloat:0], [NSNumber numberWithFloat:PI], nil];
+		[sharedLibrary addObject:water];
+	}
+	return sharedLibrary;
+}
+
+@end
+
+
+@interface GestureRecognizer : NSObject
+
+- (id)initAtStartingPos:(CGPoint)startingPos;
+- (Gesture*)newTouchAt:(CGPoint)pos failed:(BOOL*)failed;
+
+- (NSArray*)getChainedGestures;
+
+@end
+
+
+
+
+@implementation GestureRecognizer
+{
+	CGPoint points[MAX_POINTS];
+	int numPoints;
+
+	float legAngles[MAX_LEGS];
+	int numLegs;
+
+	NSMutableArray * chainedGestures;
+	BOOL failed;
+}
+
+- (id)initAtStartingPos:(CGPoint)startingPos
+{
+	if (self = [super init])
+	{
+		points[0] = startingPos;
+		numPoints = 1;
+		chainedGestures = [NSMutableArray array];
+	}
+	return self;
+}
+
 
 - (BOOL)getLatestAngle:(float*)angleOut
 {
@@ -97,10 +150,10 @@ typedef enum
 	for (int i = numPoints - 2; i >= 0; i--)
 	{
 		CGPoint currentPoint = points[i];
-		float dist = [self distanceBetweenPoint:currentPoint and:lastPoint];
+		float dist = distanceBetweenPoints(currentPoint, lastPoint);
 		if (dist > DIST_FOR_ACCURATE_ANGLE)
 		{
-			float angle = [self angleBetweenPoint:currentPoint and:lastPoint];
+			float angle = angleBetweenPoints(currentPoint, lastPoint);
 			*angleOut = angle;
 			return YES;
 		}
@@ -108,8 +161,16 @@ typedef enum
 	return NO;
 }
 
-- (void)newTouchAt:(CGPoint)pos
+- (Gesture*)newTouchAt:(CGPoint)pos failed:(BOOL*)failedOut
 {
+	*failedOut = NO;
+
+	if (failed)
+	{
+		*failedOut = YES;
+		return nil;
+	}
+	
 	if (numPoints < MAX_POINTS)
 	{
 		points[numPoints] = pos;
@@ -125,28 +186,54 @@ typedef enum
 			}
 			else
 			{
-				float angleDiff = [self getDifferenceBetweenAngle:legAngles[numLegs-1] and:currentAngle];
+				float angleDiff = getDifferenceBetweenAngles(legAngles[numLegs-1], currentAngle);
 				if (fabsf(angleDiff) > ANGLE_DIFFERENCE_FOR_NEW_LEG)
 				{
-					if (numLegs < MAX_LEGS)
+					if (numLegs >= MAX_LEGS-1)
+					{
+						failed = YES;
+						*failedOut = YES;
+						return nil;
+					}
+					else
 					{
 						legAngles[numLegs] = currentAngle;
 						numLegs++;
+						if ([self checkChainedGesture])
+						{
+							return [chainedGestures objectAtIndex:[chainedGestures count] - 1];
+						}
 					}
 				}
 			}
 		}
 	}
+	return nil;
 }
 
-- (EGesture)getGesture
+- (BOOL)checkChainedGesture
 {
-	for (int i = 0; i < numLegs; i++)
+	NSArray * gestureLibrary = [Gesture gestureLibrary];
+	for (Gesture * gesture in gestureLibrary)
 	{
-		NSLog(@"leg: %f", legAngles[i]);
+		if ([gesture matchesLegAngles:legAngles numLegs:numLegs])
+		{
+			[chainedGestures addObject:gesture];
+			numLegs = 0;
+			return YES;
+		}
+	}
+	return NO;
+}
+
+- (NSArray*)getChainedGestures
+{
+	if (failed || numLegs > 1)
+	{
+		return nil;
 	}
 
-	return EGesture_NONE;
+	return chainedGestures;
 }
 
 @end
@@ -216,18 +303,38 @@ typedef enum
 //	NSLog(@"Second touch is at %f %f" ,touchLocation.x, touchLocation.y);
     if (currentGestureRecognizer)
     {
-        [currentGestureRecognizer newTouchAt:touchLocation];
+		BOOL failed;
+        Gesture * newGesture = [currentGestureRecognizer newTouchAt:touchLocation failed:&failed];
+		if (failed)
+		{
+			NSLog(@"failed");
+		}
+		else if (newGesture)
+		{
+			NSLog(@"new gesture: %d", newGesture.gesture);
+		}
     }
 }
 
 - (void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event
 {
-    CGPoint touchLocation = [touch locationInView:touch.view];
+//    CGPoint touchLocation = [touch locationInView:touch.view];
 //	NSLog(@"3rd touch is at %f %f" ,touchLocation.x, touchLocation.y);
     if (currentGestureRecognizer)
     {
-        [currentGestureRecognizer newTouchAt:touchLocation];
-        [currentGestureRecognizer getGesture];
+        NSArray * chainedGestures = [currentGestureRecognizer getChainedGestures];
+		if (chainedGestures && [chainedGestures count] > 0)
+		{
+			NSLog(@"got gestures:");
+			for (Gesture * gesture in chainedGestures)
+			{
+				NSLog(@"    gesture: %d", gesture.gesture);
+			}
+		}
+		else
+		{
+			NSLog(@"No gestures");
+		}
         currentGestureRecognizer = nil;
     }
 }
