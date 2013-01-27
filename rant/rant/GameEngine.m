48aad794755359dafafb7301d08fa30adff8a4c4
Andy Mroczkowski <andy@mrox.net>
2013-01-27 08:26:59 -0800

    formatting

--- rant/rant/GameEngine.m
+++ rant/rant/GameEngine.m
@@ -37,7 +37,7 @@
 
 - (void)broadcastEventAsServer:(GameEvent *)event
 {
-    NSLog(@"*** [GAME] [SEND] broadcast event src=%d type=%d", event->source, event->type);
+    NSLog(@"*** [GAME] [SEND] broadcast event src=%lld type=%d", event->source, event->type);
     
     GameState state = self.currentState;
     [self.delegate clientReceivedEvent:event withState:&state];
@@ -120,7 +120,7 @@
 {
     event->source = [self myPlayerNum];
     
-    NSLog(@"*** [GAME] [SEND] client event src=%d type=%d", event->source, event->type);
+    NSLog(@"*** [GAME] [SEND] client event src=%lld type=%d", event->source, event->type);
     
 	if ([self isServer])
 	{
