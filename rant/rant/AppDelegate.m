73d1b8aaaaf2467eca5e7946070b7e9737571b5e
Tedo Salim <tedo.salim@gree.co.jp>
2013-01-27 08:20:09 -0800

    + added font and lobby characters
    Signed-off-by: Tedo Salim <tedo.salim@gree.co.jp>

--- rant/rant/AppDelegate.m
+++ rant/rant/AppDelegate.m
@@ -78,7 +78,7 @@
 	// and add the scene to the stack. The director will run it when it automatically when the view is displayed.
 //	[director_ pushScene:[GameScene scene]];
 
-    [director_ pushScene:[MainMenuScene scene]];
+    [director_ pushScene:[LobbyScene scene]];
 
 	
 	// Create a Navigation Controller with the Director
